9d0a465ffddd36d6e859d8248cc4b549b09d9b50
marwaabdelaal <marwaabdelaal112@gmail.com>
2022-08-16 12:35:09 +0200

    hbs view engine

--- session7-express/index.js
+++ session7-express/index.js
@@ -3,27 +3,23 @@ const app = express()
 const PORT = 3000
 const path = require("path")
 const staticDir = path.join(__dirname, "static") //`${__dirname}/static`
+const viewsDir = path.join(__dirname, "frontend/views") //`${__dirname}/static`
 app.use(express.static(staticDir))
 
 // view engine hbs, ejs, pug .... => npm i hbs
 app.set("view engine", "hbs")
+app.set("views", viewsDir)
 
 // route => request, response, keyword
-app.get("/", (req, res)=>{
-    res.send("Hello from express")
-})
-app.get("/about", (req, res)=>{
-    res.send({name: "marwa", age: 22})
-})
-app.get("/contact", (req, res)=>{
-    res.send("<h1>Hi</h1>")
-})
 app.get("/html", (req, res)=>{
     // const fileLoc = `${__dirname}/1.png`
     res.sendFile(staticDir)
 })
-app.get("/hbs", (req, res)=>{
+app.get("/", (req, res)=>{
     res.render("home")
+})
+app.get("/about", (req, res)=>{
+    res.render("about")
 })
 
 app.listen(PORT, ()=> console.log(`we are on http://localhost:${PORT}`)) // http://localhost:3000/
