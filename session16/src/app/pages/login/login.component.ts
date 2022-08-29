30dcd293babf22e9b11f1c2fc01ca5fe6d81587a
marwaabdelaal <marwaabdelaal112@gmail.com>
2022-08-29 15:28:41 +0200

    login form

--- session16/src/app/pages/login/login.component.ts
+++ session16/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  userData = {
+    email:"",
+    password:""
+  }
 
-  constructor() { }
+  constructor(private _data:DataService) { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(login:NgForm){
+    if(login.valid){
+      this._data.login(this.userData)
+      .subscribe(
+        data => {console.log(data), console.log("Done")},
+        e => {console.log(e), console.log("Error!")}
+      )
+    }
   }
 
 }
--- session16/src/app/pages/register/register.component.ts
+++ session16/src/app/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-register',
@@ -8,15 +9,28 @@ import { NgForm } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  handleregister(register:NgForm){
-    if(register.valid){
-      console.log(register.value)
-    }
+  userData = {
+    name:"",
+    age:0,
+    email:"",
+    password:"",
+    gender:""
   }
 
-  constructor() { }
+  constructor(private _data:DataService) { }
 
   ngOnInit(): void {
+  }
+
+  handleRegister(register:NgForm){
+    if(register.valid && this.userData.age > 20 && this.userData.age < 65){
+      // console.log(this.userData)
+      this._data.register(this.userData)
+      .subscribe(
+        data => {console.log(data), console.log("Done")},
+        e => {console.log(e), console.log("Error!")}
+      )
+    }
   }
 
 }
